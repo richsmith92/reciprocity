3731446037b486405ff3848e2e5c5d72cbcee009
Alexey Karakulov <ankarakulov@gmail.com>
2016-10-28 20:18:31 +0300

    Moved joinSources to Common

--- app/Command/Common.hs
+++ app/Command/Common.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Command.Common (module Command.Common, module Options.Applicative) where
 
 import           CustomPrelude
@@ -10,6 +12,10 @@ import System.IO (IOMode(..), withBinaryFile)
 import System.FilePath (splitExtension, addExtension)
 import Data.Conduit.Zlib (ungzip)
 import qualified Data.ByteString.Char8 as BC
+import           Data.Conduit.Internal        (ConduitM (..), Pipe (..))
+import qualified Data.Conduit.Internal        as CI
+import qualified Data.Conduit.Binary as CB
+
 --
 -- class (Show a) => ToRec a where
 --   toRec :: a -> ByteString
@@ -62,9 +68,18 @@ execSubRec Opts{..} = \case
 
 -- * Producers
 
+inputSource :: (MonadResource m) => FilePath -> Source m ByteString
+inputSource file = source .| linesUnboundedAsciiC
+-- inputSource file = source .| CB.lines
+  where
+  source = if
+    | file == "-" -> stdinC
+    | ".gz" `isSuffixOf` file -> sourceFile file .| ungzip
+    | otherwise -> sourceFile file
+
 inputSources :: MonadResource m => Opts -> [Source m ByteString]
 inputSources Opts{..} = case optsInputs of
-  [] -> [stdinC]
+  [] -> [stdinC .| linesUnboundedAsciiC]
   inputs -> map inputSource inputs
 
 inputFiles :: Opts -> [FilePath]
@@ -96,13 +111,35 @@ catHFiles = \case
 sourceDropHeader :: (MonadResource m) => FilePath -> Source m ByteString
 sourceDropHeader file = inputSource file .| tailC
 
-inputSource :: (MonadResource m) => FilePath -> Source m ByteString
-inputSource file = source .| linesUnboundedAsciiC
-  where
-  source = if
-    | file == "-" -> stdinC
-    | ".gz" `isSuffixOf` file -> sourceFile file .| ungzip
-    | otherwise -> sourceFile file
+-- TEST: joinSources (CL.sourceList [1,3..10 :: Int]) (CL.sourceList [1..5]) $$ CL.mapM_ print
+-- joinSources :: (Ord k, Monad m, StringLike a)
+  -- => (a -> k) -> (a -> v) -> (k -> v -> v -> b) -> Source m a -> Source m a -> Source m b
+joinSources fk fv combine (ConduitM !left0) (ConduitM !right0) = ConduitM $ \rest -> let
+  -- go (Done ()) r = CI.mapOutput (fk &&& That . fv) r >> rest ()
+  -- go l (Done ()) = CI.mapOutput (fk &&& This . fv) l >> rest ()
+  go xs@(HaveOutput srcx closex s1) ys@(HaveOutput srcy closey s2) = let
+    k1 = fk s1
+    k2 = fk s2
+    v1 = fv s1
+    v2 = fv s2
+    in case compare s1 s2 of
+    -- in case compare k1 k2 of
+      -- LT -> HaveOutput (go srcx ys) closex $ (k1, This v1)
+      -- GT -> HaveOutput (go xs srcy) closey $ (k2, That v2)
+      LT -> go srcx ys
+      GT -> go xs srcy
+      -- EQ -> HaveOutput (go srcx srcy) (closex >> closey) (combine k1 v1 v2)
+      EQ -> HaveOutput (go srcx srcy) (closex >> closey) s1
+  go (Done ()) r = rest ()
+  go l (Done ()) = rest ()
+  go (Leftover left ()) right = go left right
+  go left (Leftover right ())  = go left right
+  go (NeedInput _ c) right = go (c ()) right
+  go left (NeedInput _ c) = go left (c ())
+  go (PipeM mx) (PipeM my) = PipeM (liftM2 go mx my)
+  go (PipeM mx) y@HaveOutput{} = PipeM (liftM (\x -> go x y) mx)
+  go x@HaveOutput{} (PipeM my) = PipeM (liftM (go x) my)
+  in go (left0 Done) (right0 Done)
 
 -- * Transformers
 
@@ -132,6 +169,8 @@ splitSink opts inFile = if optsHeader opts then await >>= sink else sink Nothing
 
 stdoutSink :: (MonadIO m) => Consumer ByteString m ()
 stdoutSink = unlinesAsciiC .| stdoutC
+
+-- * Option parsing
 
 type OptParser a = Mod OptionFields a -> Parser a
 textOpt :: Textual s => (s -> a) -> OptParser a
--- app/Command/Join.hs
+++ app/Command/Join.hs
@@ -3,11 +3,9 @@ module Command.Join where
 import           CustomPrelude
 import           Command.Common
 
-import           System.FilePath       (addExtension, splitExtension)
-import           Data.ByteString.Internal (c2w, w2c)
-import           Data.These      (These (..), justThese)
-import           Data.Conduit.Internal        (ConduitM (..), Pipe (..))
-import qualified Data.Conduit.Internal        as CI
+-- import           System.FilePath       (addExtension, splitExtension)
+-- import           Data.ByteString.Internal (c2w, w2c)
+-- import           Data.These      (These (..), justThese)
 
 data CmdJoin = CmdJoin {
   joinValue :: SubRec
@@ -29,31 +27,3 @@ instance IsCommand CmdJoin where
       joinValue <- valueOpt
       return (CmdJoin{..})
     }
-
--- TEST: joinSources (CL.sourceList [1,3..10 :: Int]) (CL.sourceList [1..5]) $$ CL.mapM_ print
-joinSources :: (Ord k, Monad m, StringLike a)
-  => (a -> k) -> (a -> v) -> (k -> v -> v -> b) -> Source m a -> Source m a -> Source m b
-joinSources fk fv combine (ConduitM left0) (ConduitM right0) = ConduitM $ \rest -> let
-  -- go (Done ()) r = CI.mapOutput (fk &&& That . fv) r >> rest ()
-  -- go l (Done ()) = CI.mapOutput (fk &&& This . fv) l >> rest ()
-  go (Done ()) r = rest ()
-  go l (Done ()) = rest ()
-  go (Leftover left ()) right = go left right
-  go left (Leftover right ())  = go left right
-  go (NeedInput _ c) right = go (c ()) right
-  go left (NeedInput _ c) = go left (c ())
-  go (PipeM mx) (PipeM my) = PipeM (liftM2 go mx my)
-  go (PipeM mx) y@HaveOutput{} = PipeM (liftM (\x -> go x y) mx)
-  go x@HaveOutput{} (PipeM my) = PipeM (liftM (go x) my)
-  go xs@(HaveOutput srcx closex s1) ys@(HaveOutput srcy closey s2) = let
-    k1 = fk s1
-    k2 = fk s2
-    v1 = fv s1
-    v2 = fv s2
-    in case compare k1 k2 of
-      -- LT -> HaveOutput (go srcx ys) closex $ (k1, This v1)
-      -- GT -> HaveOutput (go xs srcy) closey $ (k2, That v2)
-      LT -> go srcx ys
-      GT -> go xs srcy
-      EQ -> HaveOutput (go srcx srcy) (closex >> closey) (combine k1 v1 v2)
-  in go (left0 Done) (right0 Done)
