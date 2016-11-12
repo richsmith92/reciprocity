19282fdc982c3032ec46454c44594f9b6608026a
Alexey Karakulov <ankarakulov@gmail.com>
2016-11-12 22:07:11 +0200

    Add/remove

---
+++ src/Reciprocity/Command/Builtin.hs
@@ -0,0 +1,126 @@
+module Reciprocity.Command.Builtin where
+
+import CustomPrelude
+import Reciprocity.Base
+import Reciprocity.Command.Base
+-- import Reciprocity.Hint
+-- import Reciprocity.Compile
+import Data.Char (isSpace)
+
+-- import           System.Directory             (getHomeDirectory)
+import Data.Text          (replace)
+-- import Data.Conduit.Zlib  (gzip)
+-- import Data.List.Extra    (groupSort)
+-- import System.IO          (IOMode (..), withBinaryFile)
+-- import System.Process
+
+-- * Cat
+
+data CmdCat = CmdCat deriving (Show)
+instance IsCommand CmdCat where
+
+  commandInfo = CmdInfo {
+    cmdDesc = "Concatenate inputs",
+    cmdParser = pure CmdCat
+  }
+
+  runCommand opts@Opts{..} _ = runConduitRes $ withInputSourcesH opts $
+    \header sources -> (yieldMany header >> sequence_ sources) .| stdoutC
+
+-- * Merge
+
+data CmdMerge = CmdMerge { mergeKey :: SubRec } deriving (Show)
+
+instance IsCommand CmdMerge where
+  commandInfo = CmdInfo {
+    cmdDesc = "Merge ordered inputs into ordered output",
+    cmdParser = do
+      mergeKey <- keyOpt
+      pure CmdMerge{..}
+    }
+  runCommand opts@Opts{..} CmdMerge{..} = runConduitRes $ withInputSourcesH opts $
+    \header sources -> (yieldMany header >> merge (map (.| linesC) sources) .| unlinesAsciiC) .| stdoutC
+    where
+    merge = case mergeKey of
+      [] -> mergeSourcesOn id
+      _  -> mergeSourcesOn $ execSubRec opts mergeKey
+
+-- * Join
+
+data CmdJoin = CmdJoin {
+  cmdJoinKey :: SubRec,
+  cmdJoinValue :: SubRec,
+  cmdJoinOuterLeft :: Bool,
+  cmdJoinOuterRight :: Bool
+  } deriving (Show)
+
+instance IsCommand CmdJoin where
+
+  commandInfo = CmdInfo {
+    cmdDesc = "Join ordered headerless inputs on common key",
+    cmdParser = do
+      cmdJoinKey <- keyOpt
+      cmdJoinValue <- valueOpt
+      cmdJoinOuterLeft <- switch (short '1' ++ help "Outer join on first input")
+      cmdJoinOuterRight <- switch (short '2' ++ help "Outer join on second input")
+      return (CmdJoin{..})
+    }
+
+  runCommand opts@Opts{..} CmdJoin{..} = do
+    let [s1, s2] = inputSources opts
+    runConduitRes $ joinCE joinOpts (map (.| linesCE) [s1, s2]) .| unlinesCE .| stdoutC
+    where
+    {-# INLINE joinOpts #-}
+    joinOpts = JoinOpts {
+      joinOuterLeft = cmdJoinOuterLeft,
+      joinOuterRight = cmdJoinOuterRight,
+      joinKey = execSubRec opts cmdJoinKey,
+      joinValue = execSubRec opts cmdJoinValue,
+      joinCombine = case cmdJoinKey of
+        [] -> headEx
+        _  -> \[k,v1,v2] -> k ++ sep ++ v1 ++ sep ++ v2
+      }
+    sep = fromString (unpack optsSep)
+
+-- * Split
+
+data CmdSplit = CmdSplit {
+  splitKey :: SubRec,
+  splitPartition :: Bool,
+  splitBuckets  :: Natural,
+  splitTemplate :: Text
+  } deriving (Show)
+instance IsCommand CmdSplit where
+
+  commandInfo = CmdInfo {
+    cmdDesc = "Split into multiple files: put records having key KEY into file INPUT.KEY",
+    cmdParser = do
+      splitKey <- keyOpt
+      splitPartition <- switch (long "partition" ++
+        help "MapReduce partition mode: split into buckets determined by hash of the key")
+      splitBuckets <- natOpt
+        (long "buckets" ++ help "Number of buckets" ++ value 1)
+      splitTemplate <- textOpt id (long "out" ++ value "{s}.{filename}" ++ help "Output filepath template (appended to input filename)")
+      return CmdSplit{..}
+    }
+
+  runCommand opts (CmdSplit{..}) = sequence_ $ zipWith split (inputFiles opts) (inputSources opts)
+    where
+    split file source = runConduitRes $ withHeader opts source $ \h -> linesCE .| splitCE (toFile file) h
+    fk = execSubRec opts splitKey
+    toFile inFile = \rec -> unpack $ replace "{s}" (value rec) template
+      where
+      template = replace "{filename}" (pack $ takeFileName inFile) splitTemplate
+      value = if
+        | splitPartition -> tshow . bucket (fromIntegral splitBuckets) . fk
+        | otherwise -> omap (\c -> if isSpace c then '.' else c) .decodeUtf8 . fk
+    -- let (base, ext) = splitExtension inFile in base +? BC.unpack (fk rec) +? ext
+    -- x +? y = if null x then y else if null y then x else x ++ "." ++ y
+
+-- * Replace
+
+data CmdReplace = CmdReplace {
+  replaceDictFile :: FilePath,
+  replaceDictKey :: SubRec,
+  replaceDictValue :: SubRec
+  }
---
+++ src/Reciprocity/Conduit.hs
@@ -0,0 +1,222 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Reciprocity.Conduit where
+
+import CustomPrelude
+import Reciprocity.Base
+
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Conduit.Binary   as CB
+import           Data.Conduit.Zlib     (ungzip)
+import           System.IO             (IOMode (..), withBinaryFile)
+-- import Control.Monad.Trans.Maybe (MaybeT(..))
+-- import qualified Data.Vector as V
+
+import           Data.Conduit.Internal (ConduitM (..), Pipe (..))
+import qualified Data.Conduit.Internal as CI
+import           Codec.Archive.Zip
+
+-- * Producers
+
+inputSource :: (MonadResource m) => FilePath -> Source m ByteString
+inputSource file = source
+  where
+  source = if
+    | file == "-" -> stdinC
+    | ".gz" `isSuffixOf` file -> sourceFile file .| ungzip
+    | ".zip" `isSuffixOf` file -> join $ liftIO $ withArchive file $ do
+        [entry] <- entryNames
+        getSource entry
+    | otherwise -> sourceFile file
+
+inputSources :: MonadResource m => Opts -> [Source m ByteString]
+inputSources Opts{..} = case optsInputs of
+  []     -> [stdinC]
+  inputs -> map inputSource inputs
+
+inputFiles :: Opts -> [FilePath]
+inputFiles Opts{..} = case optsInputs of
+  []     -> [""]
+  inputs -> replaceElem "-" "" inputs
+
+withInputSourcesH :: (MonadResource m, MonadTrans t, Monad (t m)) =>
+  Opts -> (Maybe ByteString -> [Source m ByteString] -> t m b) -> t m b
+withInputSourcesH opts@Opts{..} combine = if
+  | optsHeader, (_:_) <- sources -> do
+    (unzip -> (sources', finalize), header:_) <- lift $
+      unzip <$> mapM (($$+ lineAsciiC foldC) >=> _1 unwrapResumable) sources
+    x <- combine (maybeAddEOL header) sources'
+    lift $ sequence_ finalize
+    return x
+  | otherwise -> combine Nothing sources
+  where
+  sources = inputSources opts
+
+maybeAddEOL :: ByteString -> Maybe ByteString
+maybeAddEOL = nonempty Nothing (Just . (`snoc` 10))
+
+-- withHeader :: (MonadIO m, MonadTrans t, Monad (t m)) =>
+  -- Opts -> Source m ByteString -> (Maybe ByteString -> t m b) -> t m b
+withHeader Opts{..} source f = if
+  | optsHeader -> do
+    ((source', finalize), header) <- lift $ (($$+ lineAsciiC foldC) >=> _1 unwrapResumable) source
+    x <- source' .| f (maybeAddEOL header)
+    lift $ finalize
+    return x
+  | otherwise -> source .| f Nothing
+
+data JoinOpts rec sub out = JoinOpts {
+  joinOuterLeft, joinOuterRight :: Bool,
+  joinKey, joinValue :: rec -> sub,
+  joinCombine :: [sub] -> out
+  }
+
+-- | Join multiple ordered sources (currenly works only for two sources)
+joinCE :: (Ord sub, Monoid sub, Monad m) => JoinOpts rec sub out -> [Source m [rec]] -> Source m (Seq out)
+joinCE jopts@JoinOpts{..} [ConduitM !left0, ConduitM !right0] = ConduitM $ \rest -> let
+  go (HaveOutput src1 close1 chunk1) (HaveOutput src2 close2 chunk2) = let
+    (out, (lo1, lo2)) = joinLists jopts chunk1 chunk2
+    src1' = if null lo1 then src1 else CI.yield lo1 >> src1
+    src2' = if null lo2 then src2 else CI.yield lo2 >> src2
+    in HaveOutput (go src1' src2') (close1 >> close2) out
+  go l (Done ()) = when joinOuterLeft (CI.mapOutput finishL l) >> rest ()
+  go (Done ()) r = when joinOuterRight (CI.mapOutput finishR r) >> rest ()
+  go (Leftover left ()) right = go left right
+  go left (Leftover right ())  = go left right
+  go (NeedInput _ c) right = go (c ()) right
+  go left (NeedInput _ c) = go left (c ())
+  go (PipeM mx) (PipeM my) = PipeM (liftM2 go mx my)
+  go (PipeM mx) y@HaveOutput{} = PipeM (liftM (\x -> go x y) mx)
+  go x@HaveOutput{} (PipeM my) = PipeM (liftM (go x) my)
+  in go (left0 Done) (right0 Done)
+  where
+  finishL = map (\rec -> joinCombine [joinKey rec, joinValue rec, mempty]) . fromList
+  finishR = map (\rec -> joinCombine [joinKey rec, mempty, joinValue rec]) . fromList
+
+-- | Merge multiple sorted sources into one sorted producer.
+
+-- mergeSources :: (Ord a, Foldable f, Monad m) => f (Source m a) -> Producer m a
+-- mergeSources = mergeSourcesOn id
+
+-- | Merge multiple sorted sources into one sorted producer using specified sorting key.
+mergeSourcesOn :: (Ord b, Monad m) => (a -> b) -> [Source m a] -> Producer m a
+mergeSourcesOn key = mergeResumable . fmap newResumableSource . toList
+  where
+    mergeResumable sources = do
+        prefetchedSources <- lift $ traverse ($$++ await) sources
+        go [(a, s) | (s, Just a) <- prefetchedSources]
+    go [] = pure ()
+    go sources = do
+        let (a, src1) : sources1 = sortOn (key . fst) sources
+        yield a
+        (src2, mb) <- lift $ src1 $$++ await
+        let sources2 = case mb of
+                Nothing -> sources1
+                Just b  -> (b, src2) : sources1
+        go sources2
+
+-- * Transformers
+
+linesC :: Monad m => Conduit ByteString m ByteString
+linesC = CB.lines
+
+linesCE :: Monad m => Conduit ByteString m [ByteString]
+linesCE = lineChunksC .| mapC BC.lines
+
+unlinesCE :: (Monad m, MonoFoldable mono, Element mono ~ ByteString) => Conduit mono m ByteString
+unlinesCE = mapC (BC.unlines . toList)
+
+lineChunksC :: Monad m => Conduit ByteString m ByteString
+lineChunksC = await >>= maybe (return ()) go
+  where
+  go acc = if
+    | Just (_, 10) <- unsnoc acc -> yield acc >> lineChunksC
+    | otherwise -> await >>= maybe (yield acc) (go' . breakAfterEOL)
+    where
+    go' (this, next) = let acc' = acc ++ this in if null next then go acc' else yield acc' >> go next
+
+breakAfterEOL :: ByteString -> (ByteString, ByteString)
+breakAfterEOL = second uncons . break (== 10) >>> \(x, m) -> maybe (x, "") (first (snoc x)) m
+
+-- replaceConduit :: HashMap ByteString ByteString -> Conduit ByteString m ByteString
+-- dictReplaceCE ::
+dictReplaceCE :: (MapValue map ~ k, ContainerKey map ~ k, IsMap map, Functor f, Monad m) =>
+  map -> ASetter s t k k -> Conduit (f s) m (f t)
+dictReplaceCE dict subrec = mapC $ map $ over subrec $ \x -> fromMaybe x $ lookup x dict
+
+subrecFilterCE :: (IsSequence b, Monad m) => (a -> Bool) -> Getting a (Element b) a -> Conduit b m b
+subrecFilterCE p subrec = mapC $ filter $ p . view subrec
+
+-- * Consumers
+
+sinkMultiFile :: (IOData a, MonadIO m) => Sink (FilePath, a) m ()
+sinkMultiFile = mapM_C $ \(file, line) ->  liftIO $
+  withBinaryFile file AppendMode (`hPutStrLn` line)
+
+splitCE :: (MonadIO m) => (ByteString -> FilePath) -> Maybe ByteString -> Sink [ByteString] m ()
+splitCE toFile mheader = foldMCE go (mempty :: Set FilePath) >> return ()
+  where
+  go files rec = liftIO $ if
+    | file `member` files -> files <$ withBinaryFile file AppendMode (\h -> hPutStrLn h rec)
+    | otherwise -> insertSet file files <$ newfile file rec
+    where
+    file = toFile rec
+  newfile = case mheader of
+    Nothing -> \file rec -> withBinaryFile file AppendMode $ \h -> hPutStrLn h rec
+    Just header -> \file rec -> withBinaryFile file WriteMode $ \h -> hPutStrLn h header >> hPutStrLn h rec
+
+-- -- works slower than `joinCE` above
+-- joinCE :: (Ord k, Monad m) => (a -> k) -> (a -> v) -> [Source m a] -> Producer m (k, [v])
+-- joinCE key val = mergeResumable . fmap newResumableSource
+--   where
+--   mergeResumable sources = do
+--     prefetchedSources <- lift $ traverse ($$++ await) sources
+--     go $ fromList [keyRecSrc (r, s) | (s, Just r) <- prefetchedSources]
+--   go v = do
+--     let i = V.minIndexBy (comparing fst) v
+--     let (kmin, (_, src1)) = v V.! i
+--     let (kmax, _) = V.maximumBy (comparing fst) v
+--     if | kmin /= kmax -> f src1 >>= maybe (return ()) (go . (\x -> v V.// [(i, keyRecSrc x)]))
+--        | otherwise -> do
+--          let (recs, srcs) = unzip $ map snd $ toList v
+--          yield (kmin, map val recs)
+--          runMaybeT (mapM (MaybeT . f) srcs) >>= maybe (return ()) (go . fromList . map keyRecSrc)
+--   keyRecSrc (rec, src) = (key rec, (rec, src))
+--   f src1 = do
+--      (src2, mrec2) <- lift $ src1 $$++ await
+--      return $ (,src2) <$> mrec2
+
+-- * Pure helpers
+
+-- | Join two ordered lists; return joined output and leftovers
+joinLists :: forall rec sub out. (Ord sub, Monoid sub) =>
+  JoinOpts rec sub out -> [rec] -> [rec] -> (Seq out, ([rec], [rec]))
+joinLists JoinOpts{..} xs ys = go xs ys (mempty :: Seq _)
+  where
+  go [] rest = (, ([], rest))
+  go rest [] = (, (rest, []))
+  go (r1:recs1) (r2:recs2) = iter ({-# SCC key #-} joinKey r1) ({-# SCC key #-} joinKey r2) r1 r2 recs1 recs2
+  go1 _ r rest [] = (, (r:rest, []))
+  go1 !k1 r1 recs1 (r2:recs2) = iter k1 ({-# SCC key #-} joinKey r2) r1 r2 recs1 recs2
+  go2 _ r [] rest = (, ([], r:rest))
+  go2 !k2 r2 (r1:recs1) recs2 = iter ({-# SCC key #-} joinKey r1) k2 r1 r2 recs1 recs2
+  {-# INLINE iter #-}
+  iter k1 k2 r1 r2 recs1 recs2 = {-# SCC iter #-} case compare k1 k2 of
+    LT -> {-# SCC lt #-} go2 k2 r2 recs1 recs2 .! appendL k1 v1
+    GT -> {-# SCC gt #-} go1 k1 r1 recs1 recs2 .! appendR k2 v2
+    EQ -> {-# SCC eq #-} go recs1 recs2 .! append k1 v1 v2
+    where
+    v1 = {-# SCC v1 #-} joinValue r1
+    v2 = {-# SCC v2 #-} joinValue r2
+  append k v1 v2 = (`snoc` {-# SCC "combine" #-} joinCombine [k, v1, v2])
+  appendL = if joinOuterLeft then \k v -> append k v mempty else \_ _ -> id
+  appendR = if joinOuterRight then \k v -> append k mempty v else \_ _ -> id
+  {-# INLINE append #-}
+
+(.!) :: (b -> c) -> (a -> b) -> a -> c
+(.!) = (.) . ($!)
+{-# INLINE (.!) #-}
+infixr 9 .!
+
+bucket :: Int -> ByteString -> Int
+bucket n s = 1 + hash s `mod` n
