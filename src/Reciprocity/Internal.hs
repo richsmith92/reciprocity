61eadc4ce2d93d6dfefb151b2d53bcba1b56dc90
Alexey Karakulov <ankarakulov@gmail.com>
2016-11-27 12:33:07 +0200

    More methods for subrecords

--- src/Reciprocity/Internal.hs
+++ src/Reciprocity/Internal.hs
@@ -34,10 +34,29 @@ getSub sep (start, end) = {-# SCC c_subrec #-} \(B.PS fPtr off len) -> unsafePer
   where
   (start', end') = (CLong $ fromIntegral start, CLong $ fromIntegral end)
 
+
+{-# INLINE splitAfterSep #-}
+splitAfterSep :: Word8 -> Int -> ByteString -> (ByteString, ByteString)
+splitAfterSep sep k str = splitAt (len+1) str
+  where
+  B.PS _ _ len = getSub sep (0, k) str
+
+{-# INLINE splitAfter #-}
+splitAfter :: Word8 -> Int -> ByteString -> (ByteString, ByteString)
+splitAfter sep k str = (sub1, sub2)
+  where
+  sub1 = getSub sep (0, k) str
+  sub2 = B.drop (B.length sub1 + 1) str
+
+{-# INLINE replaceSub #-}
 replaceSub :: ByteString -> ByteString -> ByteString -> ByteString
 replaceSub (B.PS _ subOff subLen) newSub str@(B.PS _ off _) =
   B.take (subOff - off) str ++ newSub ++ B.drop (subOff - off + subLen) str
 
+{-# INLINE subLens #-}
 subLens :: Word8 -> (Int, Int) -> Lens' B.ByteString B.ByteString
 subLens sep bounds = \f str -> let sub = getSub sep bounds str in
   (\sub' -> replaceSub sub sub' str) <$> f sub
+
+-- uappend :: (ByteString, ByteString) -> ByteString
+-- uappend = uncurry (++)
